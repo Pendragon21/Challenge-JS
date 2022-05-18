4487003c1f321f905ac87be9bd8d0e2a700e48c0
unknown <rafaelbemvindo@hotmail.com>
2022-05-17 21:22:58 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import {comboBox,inputText,list} from './module'
+
+function handleEventText(){
+    let i = document.createElement('form')
+    let text = document.createElement('input');
+    text.setAttribute('type', 'text');
+    text.setAttribute('id', 'eventtext');
+    text.setAttribute('placeholder', 'insert a text');
+
+    i.appendChild(text);
+    document.getElementById('InputsWrapper').appendChild(i);
+
+ }
+
+ function handleEventCombo(){
+    let i = document.createElement('form')
+    let combo = document.createElement('select');
+    combo.setAttribute('id','comboBox')
+    let options = document.createElement('option');
+    combo.setAttribute('class', 'combo');
+    options.setAttribute('value', 'select');
+    options.innerHTML += "select..."
+
+    i.appendChild(combo);
+    combo.appendChild(options);
+    document.getElementById('InputsWrapper').appendChild(i);
+ }
+
+ function handleEventList(){
+    let i = document.createElement('form')
+    let unordenedList = document.createElement('ul');
+    unordenedList.setAttribute('id', 'unordenedList');
+    let list = document.createElement('li');
+    list.innerHTML += "simple list"
+
+    i.appendChild(unordenedList);
+    unordenedList.appendChild(list);
+    document.getElementById('InputsWrapper').appendChild(i);
+ }
